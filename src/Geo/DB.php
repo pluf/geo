d58b13f98a6a6beeb68bcc4b205c668261375899
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-07-13 18:47:36 +0430

    Adding a POC to show conversion form GEO data into WKT automatically.

--- src/Geo/DB.php
+++ src/Geo/DB.php
@@ -1,33 +1,15 @@
 <?php
 
-
 geoPHP::load();
 
 function Geo_DB_PointFromDb ($val)
 {
-    
-//     $oGeometry = geoPHP::load('0101000000000000000000f03f000000000000f03f','wkb', true);
-//     $reducedGeom = $oGeometry->simplify(1.5);
-//     $sWkt = $reducedGeom->out('wkt');
-    
-    $a = unpack('C*', $val);
-    
-    //'0101000000000000000000f03f000000000000f03f'
-    //'0000000001010000000000000000000000000000000000F03F
-    $hex_ary = array();
-
-    for($i = 4 ; $i < 25; $i++) {
-        $hex_ary[] = sprintf("%02X", $a[$i]);
-    }
-    $hex = implode('',$hex_ary);
-    echo $hex;
-    
-    
+    // XXX: maso, 1395: convert $val (from BLOB) to WKT  
     $wkb_reader = new WKB();
-    $geometry = $wkb_reader->read($hex,TRUE);
+    $geometry = $wkb_reader->read('0101000000000000000000f03f000000000000f03f',TRUE);
     $wkt_writer = new WKT();
-    $wkt = $wkt_writer->write(oGeometry);
-    return $Wkt;
+    $wkt = $wkt_writer->write($geometry);
+    return $wkt;
 }
 
 function Geo_DB_PointToDb ($val, $db)
