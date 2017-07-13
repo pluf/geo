0bd0d3295a1f7ca5a0eef65023fafb0013ad337c
Mostafa Barmshory <mostafa.barmshory@dpq.co.ir>
2017-07-13 21:38:57 +0430

    test

--- src/Geo/DB.php
+++ src/Geo/DB.php
@@ -4,9 +4,11 @@ geoPHP::load();
 
 function Geo_DB_PointFromDb ($val)
 {
+    echo PHP_EOL;
+    $v = unpack ("C*", $val);
     // XXX: maso, 1395: convert $val (from BLOB) to WKT  
     $wkb_reader = new WKB();
-    $geometry = $wkb_reader->read('0101000000000000000000f03f000000000000f03f',TRUE);
+    $geometry = $wkb_reader->read('010100000000008099542fc8410000001027a0da41',TRUE);
     $wkt_writer = new WKT();
     $wkt = $wkt_writer->write($geometry);
     return $wkt;
